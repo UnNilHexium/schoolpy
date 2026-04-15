79ba2177196cef670964da52a4eb2c2938206879
UnNilHexium <11199@sanskritischool.edu.in>
2026-04-15 11:13:18 +0530

    15-4-26

---
+++ Functions/return.py
@@ -0,0 +1,13 @@
+#we can return values in a funtion
+#this helps the calling statement use the returned values
+#you can return multiple values too
+#they can later be asigned global variable
+# -one variable assigned to multiple
+#       -
+# -all variables assigned one variable each
+
+def f1(n):
+    f=1
+    for i in range(1,n+1):
+        f=f*1
+    return f,g,h
