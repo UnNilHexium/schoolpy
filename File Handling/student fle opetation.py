489674136565b447a492c132a1364ba89a2ffbc0
UnNilHexium <11199@sanskritischool.edu.in>
2026-05-19 11:40:32 +0530

    for laksh

--- File Handling/student fle opetation.py
+++ File Handling/student fle opetation.py
@@ -11,24 +11,50 @@ def add_data():
     return 
 def read_data():
     f1=open("ABC.dat","rb")
+    i=1
     while True:
         try:
             s_data=pickle.load(f1)
-            print("R. no", s_data[0],"name",s_data[1],"class",s_data[2],"marks",s_data[3])
+            print(i, "|  R. no: ", s_data[0],"|    Name: ",s_data[1],"|      class: ",s_data[2],"|   marks:",s_data[3])
+            i+=1
+            
         except:
             break
+    print("number of entries = ", i)
     print("EOF reached")
     f1.close()
     return
 def search_file():
+    print("what do you want to search? \nR: roll number \nC: Class)")
+    x=input("Enter Option : ").lower()
+     if x=="r":
+        search_rno()
+    elif x=="c":
+        search_class()
+    else:
+        print("invalid input!")
+    return
+def search_rno():
+    f1=open("ABC.dat","rb")
+    r=int(input("Please enter roll number : "))
+    found=False
+    while True:
+        try:
+            s_data=pickle.load(f1)
+            if r==s_data[0]:
+                 print("|  R. no: ", s_data[0],"|    Name: ",s_data[1],"|      class: ",s_data[2],"|   marks:",s_data[3])
+        except:
+
+    return
+def search_class():
     return
 while True:
-    print("Student file operations\n    A:Add a new record\n    B:View all records\n    C:Search for a record\n     D:Exit")
-    x=input("Enter your option").lower()
+    print("Student file operations\n    A:Add a new record\n    B:View all records\n    C:Search for a record\n    D:Exit")
+    x=input("Enter your option : ").lower()
     if x=="a":   
         while True:
             add_data()
-            y=input("Add one more?")
+            y=input("Add one more? ")
             if y=="n":
                 break
     elif x=="b":
@@ -40,4 +66,3 @@ while True:
         break
     else:
         print("invalid input!")
-
