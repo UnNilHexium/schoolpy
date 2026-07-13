30a2db9b63918adf480898bc9def7530d7bfdbfb
UnNilHexium <11199@sanskritischool.edu.in>
2026-07-13 11:48:21 +0530

    00

--- File Handling/csv_file_handling.py
+++ File Handling/csv_file_handling.py
@@ -15,28 +15,54 @@ def show():
             print(data)
 
 def search():
-    book_no=int(input('Please enter book no. to search: '))
+    book_no=input('Please enter book no. to search: ')
     with open('book.csv','r') as f:
         r=csv.reader(f)
         for data in r:
             if data[0]==book_no:
                 print(data)
             
-        
+
+ def modify():
+    book_no = input('Please enter book no. to modify: ')
+    
+
+    with open('book.csv', 'r') as f:
+        r = csv.reader(f)
+        booklist = list(r)
+    
+    flag = 0
+    
+   
+    for row in booklist:
+        if row[0] == book_no:
+            current_price = float(row[1])
+            new_price = current_price + (current_price * 0.1)  
+            row[1] = str(round(new_price, 2)) 
+            flag = 1
+            break  
+            
+    if flag == 0:
+        print('Record not found')
+    else:
+        with open("book.csv", 'w', newline='') as f:
+            w = csv.writer(f)
+            w.writerows(booklist)
+
 def modify():
-    book_no=int(input('Please enter book no. to modify: '))
+    book_no=input('Please enter book no. to modify: ')
     with open('book.csv','r') as f:
         r=csv.reader(f)
         booklist=list(r)
         i=0
         flag=0
         while True:
-            if booklist[i][0]==book_no:
+            if str(booklist[i][0])==book_no:
                 booklist[i][0]+=booklist[i][0]*0.1
                 flag=1
                 break
             else:
-                i+=0
+                i+=1
         if flag==0:
             print('record not found')
         else:
